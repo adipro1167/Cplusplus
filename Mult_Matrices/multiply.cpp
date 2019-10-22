6a972539cec8d882cec87acf244127c92558046d
adipro1167 <56895297+adipro1167@users.noreply.github.com>
2019-10-23 02:18:33 +0530

    MULTIPLY MATRICES

--- Mult_Matrices/multiply.cpp
+++ Mult_Matrices/multiply.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
 int main(){
-  int matrice_1 [10][10];
-  int matrice_2 [10][10];
+  int matrice_1 [1000][1000];
+  int matrice_2 [1000][1000];
   int rows_1, rows_2, cols_1, cols_2;
-
-  int matrice_output [10][10];
-
+  int matrice_output [1000][1000];
   int temp_mult = 0;
   cout << "Enter rows and columns for first matrix: ";
   cin >> rows_1 >> cols_1;
   cout << "Enter rows and columns for second matrix: ";
   cin >> rows_2 >> cols_2;
-
   cout << endl << "Enter elements of matrix 1:" << endl;
   for(int i = 0; i < rows_1; ++i){
     for(int j = 0; j < cols_1; ++j){
@@ -22,8 +17,6 @@ int main(){
       cin >> matrice_1[i][j];
       }
   }
-
-    // Storing elements of second matrix.
   cout << endl << "Enter elements of matrix 2:" << endl;
   for(int i = 0; i < rows_2; ++i){
     for(int j = 0; j < cols_2; ++j){
@@ -31,7 +24,6 @@ int main(){
       cin >> matrice_2[i][j];
       }
   }
-
   if (rows_2 != cols_1 || rows_1 != cols_2){
     cout << "Can't multiply matrices" << endl;
     return 0;
@@ -45,7 +37,6 @@ int main(){
       }
     }
   }
-
   cout << "\nResults: \n" << endl;
   for(int i = 0; i < cols_2; i++){
     cout << "[ ";
