e3c11e298714f1f3ffb7368374ea5addb7c8d59e
adipro1167 <56895297+adipro1167@users.noreply.github.com>
2019-10-23 02:13:17 +0530

    ADDITION OF MATRICES

---
+++ matrix-add.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int rows;
+	int columns;
+	
+	int matrix1[1000][1000];
+	int matrix2[1000][1000];
+	int matrixSum[1000][1000];
+	int i, j;
+	cout << "Welcome To Addition Between Two Matrices" << endl;
+	cout << "Enter number of rows:";
+	cin >> rows;
+	cout << "Enter number of columns:";
+	cin >> columns;
+	cout << endl;
+	cout << "Matrix 1 Elements are: " << endl;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < columns; j++)
+		{
+			cout << "Element " << "[" << i + 1 << "," << j + 1 << "]" << ":           ";
+			cin >> matrix1[i][j];
+		}
+	}
+	cout << endl;
+	cout << "Matrix 2 Elements are: " << endl;
+	for (i = 0; i < rows; ++i)
+	{
+		for (j = 0; j < columns; j++)
+		{
+			cout << "Element " << "[" << i + 1 << "," << j + 1 << "]" << ":           ";
+			cin >> matrix2[i][j];
+		}
+	}
+	
+	cout << endl;
+
+	cout << "Matrix Sum Elements: " << endl;
+
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < columns; j++)
+		{
+			matrixSum[i][j] = matrix1[i][j] + matrix2[i][j];
+			cout << matrixSum[i][j] << " ";
+
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
